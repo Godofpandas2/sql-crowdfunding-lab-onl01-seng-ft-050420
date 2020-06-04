d9d67a54d96192cbc716cdab4f9a2d1712cc9c3e
David Hutchison <dhutchis68516@gmail.com>
2020-06-04 16:49:19 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount) FROM pledges
-  INNER JOIN project ON pledges.project_id = projects.id"
+  INNER JOIN projects ON pledges.project_id = projects.id"
 
 end
 
